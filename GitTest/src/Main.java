022958cf1420b93f45524fbfb252d3bb615cf7d2
Juli86 <Vanil.ka@mail.com>
2013-08-21 13:25:25 +0300

    Update Main.java

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("ACCAA");
+        System.out.println("еуея");
         System.out.println();
         System.out.println("hello");
     }
